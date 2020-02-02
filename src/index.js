22ad77bed8028e37893cd27bbdec4008da56e8e0
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-02 01:45:03 -0500

    Added React Router's Router Component to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import {BrowserRouter as Router } from 'react-router-dom';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -17,7 +18,9 @@ console.log(store.getState());
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>
 
 , document.getElementById('root'));
