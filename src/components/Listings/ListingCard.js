5b34f3c130069e706ed3f8351581b518eec065d6
Kevin <knphillips01@gmail.com>
2020-02-05 16:17:51 -0600

    added some styling

--- src/components/Listings/ListingCard.js
+++ src/components/Listings/ListingCard.js
@@ -1,24 +1,31 @@
 import React from 'react';
-
 import {
   Card,
-  Grid,
-  CardContent
-} from '@material-ui/core';
+  Button,
+  CardHeader,
+  CardFooter,
+  CardBody,
+  CardTitle,
+  CardText,
+  Col
+} from "reactstrap";
+
 
 const ListingCard = props => {
 
   return (
-    <>
-      <p>{props.listing.title} </p>
-      <Grid>
-        <Card>
-          <CardContent>
-           {props.listing.title}
-          </CardContent>
-        </Card>
-      </Grid>
-    </>
+    <div>
+      <>
+      <Card>
+        <CardBody>
+          <CardTitle>{props.listing.title}</CardTitle>
+          <CardText>Location: {props.listing.location}</CardText>
+          <CardText>Description: {props.listing.description}</CardText>
+          <CardText>Price: ${props.listing.price_per_day} </CardText>
+        </CardBody>
+      </Card>
+      </>
+    </div>
   )
 };
 
--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 import ListingCard from './ListingCard';
 import { results } from '../../api/data';
+import { Container, Row } from "reactstrap";
+
 
 const Listings = () => {
     const [data] = useState(results);
     
     return(
-        <div>
+        <Container>
+            <Row>
             {
                 data.data.map(listing => (
                 <div key={listing.id}>
-                     <ListingCard listing={Listings}/>
+                    <ListingCard listing={listing}/>
                 </div>
                 
                 ))
             }
-        </div>
+            </Row>
+        </Container>
     )
 }
 
