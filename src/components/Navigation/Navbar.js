d9b4cefda4db2383274aabcef50654fda177e3af
Kevin <knphillips01@gmail.com>
2020-02-05 21:35:21 -0600

    removed material

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/ToolBar';
-import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom'
+// import AppBar from '@material-ui/core/AppBar';
+// import ToolBar from '@material-ui/core/ToolBar';
+// import Typography from '@material-ui/core/Typography';
+import { Navbar } from 'reactstrap';
 
-const NavBar = () => {
+const NavigationBar = () => {
   return(
     <div>
-      <AppBar position = "static">
-        <ToolBar>
-          <Typography>
-            RVNB
-          </Typography>
-        </ToolBar>
-      </AppBar>
+      <Navbar>
+        <Link to='/dashboard'>Dashboard</Link>
+      </Navbar>
     </div>
   )
 }
   
-export default NavBar;
+export default NavigationBar;
