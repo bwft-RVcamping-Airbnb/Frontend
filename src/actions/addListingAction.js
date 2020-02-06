d090ad0b96e048dee3459e333fdba6f3dc710a65
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-05 21:14:26 -0500

    Adding AddListing Form, reducer, action

---
+++ src/actions/addListingAction.js
@@ -0,0 +1,9 @@
+import {ADDING_LISTING, ADDING_LISTING_SUCCESS ,ADDING_LISTING_FAILURE} from '../actions/index';
+import { push } from 'connected-react-router';
+
+
+export const addListingAction = () => {
+    return dispatch => {
+        
+    }
+}
---
+++ src/reducers/addListingReducer.js
@@ -0,0 +1,33 @@
+import {ADDING_LISTING, ADDING_LISTING_SUCCESS ,ADDING_LISTING_FAILURE} from '../actions/index';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+
+const initialState = {
+    isLoading: false,
+    errors: '',
+    message: ''
+}
+
+export const AddListingReducer = (state = initialState, action) => {
+
+        switch(action.type){
+            case ADDING_LISTING:
+                return{
+                    ...state,
+                    isLoading: !state.isLoading
+                }
+            case ADDING_LISTING_SUCCESS:
+                return{
+                    ...state,
+                    isLoading: !state.isLoading,
+                    message: action.payload
+                }
+            
+                case ADDING_LISTING_FAILURE:
+                    return{
+                        ...state,
+                        errors: action.payload
+                    }
+            default:
+                return state
+        }
+}
